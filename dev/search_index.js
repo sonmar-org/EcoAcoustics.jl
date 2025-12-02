3876220a4d34f39bcb6f57d46442dc5212f64b67
Documenter.jl <documenter@juliadocs.github.io>
2025-12-02 20:35:00 +0000

    build based on da2a537

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#EcoAcoustics","page":"Home","title":"EcoAcoustics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EcoAcoustics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#EcoAcoustics","page":"Home","title":"EcoAcoustics","text":"Documentation for EcoAcoustics.\n\n","category":"section"}]
 }
